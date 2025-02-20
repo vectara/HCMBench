826c359cd54edf45027ec8dbe20a18b9355772aa
Rogger Luo <rogger@vectara.com>
2025-02-20 00:35:18 +0000

    add hhem & minicheck

---
+++ Minicheck.py
@@ -0,0 +1,20 @@
+from Evaluator import EvaluationModel
+import torch
+
+class Minicheck(EvaluationModel):
+    """Minicheck model for evaluating generated output.
+    """
+    def __init__(self, model_name="Bespoke-MiniCheck-7B"):
+        from minicheck.minicheck import MiniCheck
+        self.scorer = MiniCheck(model_name=model_name)
+
+    def predict_one(self, claim: str, context: str):
+        pred_label, raw_prob, _, _ = scorer.score(docs=[context], claims=[claim]) 
+        return raw_prob[0]
+
+if __name__ == '__main__':
+    model = Minicheck()
+    claim = "The sky is blue."
+    context = "The sky is blue because of the way the Earth's atmosphere scatters sunlight."
+    score = model.predict_one(claim, context)
+    print(score)
