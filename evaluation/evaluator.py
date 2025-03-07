1d4d6296432b43d819b66c1803cab47458c2c41f
Rogger Luo <rogger@vectara.com>
2025-03-07 00:22:19 +0000

    fix

--- evaluation/evaluator.py
+++ evaluation/evaluator.py
@@ -16,8 +16,8 @@ class EvaluationModel:
     """A class to evaluate generated output.
     """
 
-    def __init__(self, model_name, claim_column = 'corrected', context_column = 'context', **kwargs):
-        self.model_name = model_name
+    def __init__(self, model_name, revision='', claim_column='corrected', context_column='context', **kwargs):
+        self.model_name = model_name + revision
         self.claim_column = claim_column
         self.context_column = context_column
     
