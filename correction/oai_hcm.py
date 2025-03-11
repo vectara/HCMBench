ac777645d9c60121dc58fc56873e33a12675a9dc
Rogger <rogger@vectara.com>
2025-03-11 13:38:33 -0700

    add vectara hcm

--- correction/oai_hcm.py
+++ correction/oai_hcm.py
@@ -8,13 +8,15 @@ sys.path.append(parent)
 from utils import get_LLM_response
 
 class OAI_HCM(CorrectionModel):
-    def __init__(self, model_name, user_prompt, system_prompt=None, RPS=20):
+    def __init__(self, model_name, base_url, model, user_prompt, system_prompt=None, RPS=20):
         super().__init__(model_name)
         self.system_prompt=system_prompt
         self.user_prompt=user_prompt
         self.RPS=RPS
+        self.base_url = base_url
+        self.model = model
 
-    def correct_one(self, claim:str, context:str):
+    def correct_one(self, claim:str, context:str, debug=False):
         messages = []
         if self.system_prompt:
             messages.append({"role": "system", "content": self.system_prompt})
@@ -24,10 +26,10 @@ class OAI_HCM(CorrectionModel):
             model = self.model, 
             messages = messages,
             temperature=0.0,
-            frequency_penalty=1,
             max_tokens=1000)
         time.sleep(1.0/self.RPS)
         llm_return = completion.choices[0].message.content
+        if debug: print(llm_return)
         return CorrectionOutput(
             claim=claim,
             context=context,
