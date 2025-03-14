07a8a2dfe18a87148e6acc5393edfca08e7b1f5f
Rogger <rogger@vectara.com>
2025-03-14 14:20:08 -0700

    add batch process for minicheck & hhem

--- pipeline/__init__.py
+++ pipeline/__init__.py
@@ -7,6 +7,7 @@ from .evaluation.rouge import Rouge
 from .evaluation.hhem import HHEM
 from .evaluation.minicheck import Minicheck
 from .evaluation.axcel import AXCEL
+from .evaluation.factsgrounding import FACTSGJudge
 
 from .preprocess.claim_extraction import ClaimExtractor
 from .preprocess.sentence_split import Sentencizer
